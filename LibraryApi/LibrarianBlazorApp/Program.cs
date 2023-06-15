22e22c556d356418d3cb4c9e69275d5f3411c6f1
Terjék Béla Szabolcs <terjekszabolcs02@gmail.com>
2023-06-15 23:03:26 +0200

    feat: add new service (MemberService)

--- LibraryApi/LibrarianBlazorApp/Program.cs
+++ LibraryApi/LibrarianBlazorApp/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LibrarianBlazorApp;
+using LibrarianBlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
+
+builder.Services.AddScoped<IMemberService,MemberService>();
 
 await builder.Build().RunAsync();
