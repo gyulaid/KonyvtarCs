84940902ec8bfd45388ca76caf3b7fa5562db03f
Terjék Béla Szabolcs <terjekszabolcs02@gmail.com>
2023-06-15 23:01:38 +0200

    feat: create service for fetching member data

---
+++ LibraryApi/LibrarianBlazorApp/Services/IMemberService.cs
@@ -0,0 +1,15 @@
+﻿using LibrarianBlazorApp.Pages;
+using LibraryApi.Contracts.Book;
+using LibraryApi.Contracts.Member;
+
+namespace LibrarianBlazorApp.Services
+{
+    public interface IMemberService
+    {
+        Task<IEnumerable<MemberResponseDto>?> GetAllMembersAsync();
+
+        Task<MemberResponseDto?> GetMemberByIdAsync(int id);
+
+        Task<IEnumerable<BookLendingDetailsDto>?> GetBooksLentByMember(int id);
+    }
+}
---
+++ LibraryApi/LibrarianBlazorApp/Services/MemberService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using LibrarianBlazorApp.Pages;
+using LibraryApi.Contracts.Book;
+using LibraryApi.Contracts.Member;
+
+namespace LibrarianBlazorApp.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly HttpClient _httpClient;
+        
+        public MemberService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<MemberResponseDto>?> GetAllMembersAsync() =>
+            _httpClient.GetFromJsonAsync<IEnumerable<MemberResponseDto>>("Members");
+
+        public Task<MemberResponseDto?> GetMemberByIdAsync(int id) =>
+            _httpClient.GetFromJsonAsync<MemberResponseDto?>($"Members/{id}");
+
+        public Task<IEnumerable<BookLendingDetailsDto>?> GetBooksLentByMember(int id) =>
+            _httpClient.GetFromJsonAsync<IEnumerable<BookLendingDetailsDto>>($"books/lent/member/{id}");
+    }
+}
