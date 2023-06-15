30600998de92ccf6d66fbd31cd6490ba71dc1f4d
Terjék Béla Szabolcs <terjekszabolcs02@gmail.com>
2023-06-15 22:59:29 +0200

    refactor: make DateofBirth nullable

--- LibraryApi/LibraryApi.Contracts/Member/MemberResponseDto.cs
+++ LibraryApi/LibraryApi.Contracts/Member/MemberResponseDto.cs
@@ -5,5 +5,5 @@ public class MemberResponseDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public DateTime? DateOfBirth { get; set; }
 }
