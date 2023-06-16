46efb0d9e7b58508df374833ed7e23d2bbf37529
Terjék Béla Szabolcs <terjekszabolcs02@gmail.com>
2023-06-16 12:06:10 +0200

    feat: worked on the Book endpoints

---
+++ LibraryApi/LibrarianBlazorApp/Services/BookService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+using LibraryApi.Contracts.Book;
+
+namespace LibrarianBlazorApp.Services;
+
+public class BookService : IBookService
+{
+    private readonly HttpClient _httpClient;
+    
+    public BookService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<BookResponseDto>?> GetAllBooksAsync() =>
+        await _httpClient.GetFromJsonAsync<IEnumerable<BookResponseDto>>("books");
+    
+    public async Task<BookLendingDetailsDto?> GetBookByIdAsync(int id) =>
+        await _httpClient.GetFromJsonAsync<BookLendingDetailsDto>($"books/{id}/lending-details");
+
+
+
+}
---
+++ LibraryApi/LibrarianBlazorApp/Services/IBookService.cs
@@ -0,0 +1,11 @@
+﻿using LibraryApi.Contracts.Book;
+
+namespace LibrarianBlazorApp.Services;
+
+
+public interface IBookService
+{
+    Task<IEnumerable<BookResponseDto>?> GetAllBooksAsync();
+
+    Task<BookLendingDetailsDto?> GetBookByIdAsync(int id);
+}
